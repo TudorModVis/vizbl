83d1e270b4dd5356b7daa72c4c3b0954b4a8563f
Cebotarenco Tudor <tudorcebotarenco@gmail.com>
2024-04-04 16:42:29 +0300

    show data

--- app/(account)/login/[[...tokens]]/page.tsx
+++ app/(account)/login/[[...tokens]]/page.tsx
@@ -67,6 +67,7 @@ export default function login ({ params }: { params: { tokens: string[]} }) {
         .then(res => res.json())
         .then(data => {
             if (data.id !== undefined) {
+                console.log(data);
                 if (params.tokens === undefined) {
                     router.push("/authenticated");
                 } else {
