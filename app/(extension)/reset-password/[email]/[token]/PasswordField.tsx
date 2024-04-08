3513d7d3e75b3e61024fc4d43c6f1f66f9baae55
Cebotarenco Tudor <tudorcebotarenco@gmail.com>
2024-04-08 21:17:32 +0300

    reset password 2

--- app/(extension)/reset-password/[email]/[token]/PasswordField.tsx
+++ app/(extension)/reset-password/[email]/[token]/PasswordField.tsx
@@ -97,7 +97,7 @@ const StyledLabel = styled('label')(({ theme }) => ({
                 color: (error !== null) ? "#C41C1C" : "#f1f1f1",
                 borderRadius: "8px",
                 marginBottom: "1.3rem",
-                fontSize: '16px',
+                fontSize: '16px !important',
                 width: '100%',
                 paddingLeft: '1.5rem'
               }}
