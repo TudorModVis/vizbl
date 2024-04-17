411e871b56a54365e9897d8d997c56e441a6b9fc
Cebotarenco Tudor <tudorcebotarenco@gmail.com>
2024-04-17 21:25:36 +0300

    layout update

--- app/(account)/layout.tsx
+++ app/(account)/layout.tsx
@@ -1,45 +1,9 @@
 import '@/styles/globals.css'
 import '@/styles/extension.css'
 import { ReactNode } from 'react'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
-export const metadata = {
-  metadataBase: new URL('https://myvizbl.com/'),
-  title: "ViZBL | Get to Know Your Friends",
-  description: 'Ever wondered what your friends watch on YouTube? Well, the ViZBL Extension allows you to share that!',
-  twitter: {
-    card: 'summary_large_image',
-    title: 'ViZBL | Get to Know Your Friends',
-    description: 'Ever wondered what your friends watch on YouTube? Well, the ViZBL Extension allows you to share that!',
-    images: ['https://myvizbl.com/twitter-image.jpg'],
-  },
-  openGraph: {
-    title: 'ViZBL | Get to Know Your Friends',
-    description: 'Ever wondered what your friends watch on YouTube? Well, the ViZBL Extension allows you to share that!',
-    type: 'website',
-    url: 'https://myvizbl.com',
-    images: ['/opengraph-image.jpg'],
-  },
-}
-const ExtensionLayout = ({ children } : { children: ReactNode }) => {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID !== undefined ? process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID : '';
-  return (
-    <html lang='en'>
-        <head>
-          <meta name="viewport" content="height=device-height, 
-                        width=device-width, initial-scale=1.0, 
-                        minimum-scale=1.0, maximum-scale=1.0, 
-                        user-scalable=no, target-densitydpi=device-dpi"/>
-        </head>
-
-          <body className='bg-body-bg relative'>
-            <GoogleOAuthProvider clientId={clientId}>
-                {children}
-            </GoogleOAuthProvider>
-          </body>
-
-    </html>
-  )
+const AccountLayout = ({ children } : { children: ReactNode }) => {
+  return <>{children}</>;
 }
 
-export default ExtensionLayout;
+export default AccountLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { ReactNode } from 'react'
 
 import { SmoothScroll} from '@/components'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 export const metadata = {
   metadataBase: new URL('https://myvizbl.com/'),
@@ -23,22 +24,17 @@ export const metadata = {
 }
 
 const RootLayout = ({ children } : { children: ReactNode }) => {
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID !== undefined ? process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID : '';
 
   return (
     <html lang='en'>
-        <head>
-          <meta name="viewport" content="height=device-height, 
-                        width=device-width, initial-scale=1.0, 
-                        minimum-scale=1.0, maximum-scale=1.0, 
-                        user-scalable=no, target-densitydpi=device-dpi"/>
-        </head>
-
           <body className='bg-body-bg relative'>
-            <SmoothScroll>
-                {children}
-            </SmoothScroll>
+            <GoogleOAuthProvider clientId={clientId}>
+              <SmoothScroll>
+                    {children}
+                </SmoothScroll>
+            </GoogleOAuthProvider>
           </body>
-
     </html>
   )
 }
