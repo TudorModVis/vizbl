5b3e130a652b5ebe5e8d8cfb34d4953eac1b1e0e
VadimOMEGA <vadimivanov.0320@gmail.com>
2024-04-18 15:26:01 +0300

    test

--- components/DashboardComponents/Name/DisplayName.tsx
+++ components/DashboardComponents/Name/DisplayName.tsx
@@ -18,16 +18,18 @@ const DisplayName = () => {
         if(userData) setName(userData.name)
     }, [userData])
 
-    if(!userData) return null
-
   return (
   <>
-    <NameModal 
-        showModal={showModal} 
-        setShowModal={setShowModal}
-        name={name}
-        setName={setName}
-    />
+  {
+    userData && (
+        <NameModal 
+            showModal={showModal} 
+            setShowModal={setShowModal}
+            name={name}
+            setName={setName}
+        />
+    )
+  }
     <div className="w-full">
         <div className="flex gap-[0.5rem] items-center">
             <p className="uppercase text-gray-border text-[1.125rem] leading-[1.25rem] font-bold">Display name</p>
