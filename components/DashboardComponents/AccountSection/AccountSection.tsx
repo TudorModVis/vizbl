2d78d087ad156fc2ad4b52f8322ddc50ed94ac0c
VadimOMEGA <vadimivanov.0320@gmail.com>
2024-04-26 00:03:13 +0300

    testing delete account

--- components/DashboardComponents/AccountSection/AccountSection.tsx
+++ components/DashboardComponents/AccountSection/AccountSection.tsx
@@ -1,19 +1,20 @@
 import DeleteAccBtn from "./DeleteAccBtn"
 import LogOutBtn from "./LogOutBtn"
+import CardLoader from "../CardLoader"
 
 
 const AccountSection:React.FC = () => {
 
   return (
-    <div className='w-full p-[1.5rem] boder-gray-border border rounded-[0.5rem] bg-card-bg flex gap-[1.5rem] mt-[1.5rem]'>
-      <div>
-        <h3 className='uppercase text-[1.125rem] font-bold text-gray-border'>ACCOUNT MANAGMENT</h3>
-        <LogOutBtn />
-        <h3 className='uppercase text-[1.125rem] font-bold text-gray-border'>DeactivatION</h3>
-        <p className='font-[500] text-[1rem] text-gray-border mt-[0.5rem]'>This account will no longer be available, and all your <br /> saved data will be permanently deleted.</p>
-        <DeleteAccBtn />
-      </div>
-    </div>
+    <CardLoader>
+        <div>
+          <h3 className='uppercase text-[1.125rem] font-bold text-gray-border'>ACCOUNT MANAGMENT</h3>
+          <LogOutBtn />
+          <h3 className='uppercase text-[1.125rem] font-bold text-gray-border'>DeactivatION</h3>
+          <p className='font-[500] text-[1rem] text-gray-border mt-[0.5rem]'>This account will no longer be available, and all your <br /> saved data will be permanently deleted.</p>
+          <DeleteAccBtn />
+        </div>
+    </CardLoader>
   )
 }
 
