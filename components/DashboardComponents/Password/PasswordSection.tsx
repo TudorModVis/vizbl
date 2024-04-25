49640783a73bd5d805421d601da791d864e3c601
VadimOMEGA <vadimivanov.0320@gmail.com>
2024-04-25 16:24:16 +0300

    testing forgot password

--- components/DashboardComponents/Password/PasswordSection.tsx
+++ components/DashboardComponents/Password/PasswordSection.tsx
@@ -14,7 +14,7 @@ const PasswordSection:React.FC = () => {
     <CardLoader>
         <div>
           {
-            userData!.authMethod === 'password' && (
+            userData && userData.authMethod === 'password' && (
               <div>
                 <h3 className='uppercase text-[1.125rem] font-bold text-gray-border'>PASSWORD</h3>
                 <p className='font-[500] text-[1rem] text-gray-border mt-[0.5rem]'>You can create a new password if you forgot it. You also can change your currently used password to a new one. (you remember it, right?)</p>
@@ -24,7 +24,7 @@ const PasswordSection:React.FC = () => {
             )
           }
           {
-            userData!.authMethod !== 'password' && (
+            userData && userData.authMethod !== 'password' && (
               <div>
                 <h3 className='uppercase text-[1.125rem] font-bold text-gray-border mt-[2rem]'>ADD A PASSWORD</h3>
                 <p className='font-[500] text-[1rem] text-gray-border mt-[0.5rem]'>Account is registered with Google, but you can enhance security by adding a password to facilitate login through both email and password credentials.</p>
