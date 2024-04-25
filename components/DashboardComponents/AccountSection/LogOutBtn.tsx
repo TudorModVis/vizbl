807c78df82e95b643a586afe1ffe6e710df31de6
VadimOMEGA <vadimivanov.0320@gmail.com>
2024-04-25 17:56:02 +0300

    testing log out

--- components/DashboardComponents/AccountSection/LogOutBtn.tsx
+++ components/DashboardComponents/AccountSection/LogOutBtn.tsx
@@ -1,14 +1,19 @@
 'use client'
-
-import { deleteCookie } from "cookies-next"
 import { useRouter } from "next/navigation"
 
 const LogOutBtn = () => {
     const router = useRouter()
 
-    const handleClick = () => {
-        deleteCookie('vizbl-session', { path: '/', domain: 'myvizbl.com' })
-        router.replace('/?loaded=true')
+    const handleClick = async () => {
+        fetch("https://api.myvizbl.com/api/logout", {
+              method: "POST",
+          })
+          .then(res => {
+              if (res.status === 401) {
+                router.replace('/login')
+              }
+          })
+          .catch(error => console.error('Error when /api/log-out: ', error));
     }
 
   return (
