812e598432588dce3fbc68c7560ea1c31a5dc213
VadimOMEGA <vadimivanov.0320@gmail.com>
2024-04-26 18:33:45 +0300

    i'm tired of giving names to commits. Updating things

--- components/DashboardComponents/Name/NameModal.tsx
+++ components/DashboardComponents/Name/NameModal.tsx
@@ -81,7 +81,7 @@ const NameModal: React.FC<NameModalProps> = ({ showModal, setShowModal, name, se
         {
             showModal && (
                 <motion.div
-                    className="bg-slate-900/20 backdrop-blur p-8 fixed inset-0 z-[5000] grid place-items-center"
+                    className="bg-slate-900/20 backdrop-blur p-[1rem] fixed inset-0 z-[5000] grid place-items-center"
                     initial={{
                         opacity: 0
                     }}
@@ -99,7 +99,7 @@ const NameModal: React.FC<NameModalProps> = ({ showModal, setShowModal, name, se
                         exit={{ scale: 0, rotate: "0deg" }}
                         onClick={(e) => e.stopPropagation()}
                         onMouseDown={(e) => e.stopPropagation()}
-                        className="w-[40rem] p-[1rem] bg-card-bg border border-gray-border text-center rounded-[0.5rem] cursor-default"
+                        className="ss:w-[40rem] w-full p-[1rem] bg-card-bg border border-gray-border text-center rounded-[0.5rem] cursor-default"
                     >
                         <h3 className="text-custom-white font-bold text-[1.5rem]">Change your display name</h3>
                         <p className="font-[500] text-[1.125rem] text-gray-border mt-[0.5rem]">Enter a new display name.</p>
