6e8f94ea08c6481c87ebd0ee43dda716414b124e
VadimOMEGA <vadimivanov.0320@gmail.com>
2024-04-26 14:27:41 +0300

    testing if font family error was solved

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import 'react-image-crop/dist/ReactCrop.css'
+
 import { NextPage } from 'next'
 import React from 'react'
 import { Footer, ScrollToTop } from "@/components"
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import '@/styles/globals.css'
-import 'react-image-crop/dist/ReactCrop.css'
 import { ReactNode } from 'react'
 
 import { SmoothScroll} from '@/components'
