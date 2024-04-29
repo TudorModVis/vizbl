81395174331419f1d1cff28cba609033a85e0df4
VadimOMEGA <vadimivanov.0320@gmail.com>
2024-04-30 02:10:33 +0300

    invite modal

--- components/DashboardComponents/Name/InviteModal.tsx
+++ components/DashboardComponents/Name/InviteModal.tsx
@@ -12,6 +12,18 @@ interface InviteModalProps {
 }
 
 const InviteModal:React.FC<InviteModalProps> = ({ showInviteModal, setShowInviteModal, username }) => {
+
+    const handleClick = () => {
+        const link = "myvizbl.com/invite/" + username.substring(1);
+        navigator.clipboard.writeText(link)
+            .then(() => {
+                console.log('Link copied to clipboard:', link);
+            })
+            .catch(err => {
+                console.error('Failed to copy link to clipboard:', err);
+            });
+    };
+
   return (
     <AnimatePresence>
         {
@@ -50,11 +62,27 @@ const InviteModal:React.FC<InviteModalProps> = ({ showInviteModal, setShowInvite
                         <h3 className="text-custom-white font-bold text-[1.5rem]">INVITE YOUR FRIENDS</h3>
                         <p className="font-[500] text-[1.125rem] text-gray-border mt-[0.5rem]">Share this link with others to add them as friends. (also get rewarded with emojis for that)</p>
 
-                        <div className="w-full h-[3rem] mt-[1.5rem] rounded-[0.25rem] border border-custom-white grid place-content-center">
+                        <div className="w-full h-[3rem] font-[500] text-custom-white text-[1.125rem] mt-[1.5rem] rounded-[0.25rem] border border-custom-white grid place-content-center">
                             {
                                 "myvizbl.com/invite/" + username.substring(1)
                             }
                         </div>
+
+                        <motion.button
+                            initial={{
+                                background: 'linear-gradient(93deg, rgba(253,162,255,1) -64.38%, rgba(120,42,213,1) 48.4%, rgba(82,184,255,1) 158.85%), rgba(0,0,0,0)'
+                            }}
+                            whileHover={{
+                                background: 'linear-gradient(93deg, rgba(30,30,30,1) -64.38%, rgba(30,30,30,1) 48.4%, rgba(30,30,30,1) 158.85%), rgba(0,0,0,0)'
+                            }}
+                            transition={{
+                                duration: 0.4
+                            }}
+                            className="py-[0.5rem] px-[1rem] border border-gray-border grid place-content-center rounded-[0.25rem]"
+                            onClick={handleClick}
+                        >
+                            Copy to clipboard
+                        </motion.button>
                     </motion.div>
                 </motion.div>
             )
