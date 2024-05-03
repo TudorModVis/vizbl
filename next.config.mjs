be9ed09011c1dfbc8130ff1e01929fcb8e123a52
Cebotarenco Tudor <tudorcebotarenco@gmail.com>
2024-05-03 15:42:57 +0300

    allow fonts

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const cspHeader = `
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
-    font-src 'self';
+    font-src * data: blob: 'unsafe-inline';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
